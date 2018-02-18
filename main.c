4ff6da385198b72f55bde006fa2be6f49aeca9b1
gourab yousuf basir <gourabxz@gmail.com>
2018-02-18 23:18:37 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,84 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+main(){
+    char days[6][14] = {"saturday", "sunday", "monday", "tuesday", "thursday", "wednessday"};
+   
+    int count = 0; char *file;
+    for (count=0; count<6; count++){
+        if( access( days[count], F_OK ) != -1 ) {
+            printf("\n\n %s class schedule: \n \n", days[count]);
+            readData(days[count]);
+            
+        } else {
+            printf("\n\n\n %s class schedule not found.\n Do you want to add schedule for %s classes? (y/n)", days[count],  days[count]);
+            char dici[100];
+            scanf("%s", &dici);
+            if((strcmp(dici, "y"))==0){
+                writeData(days[count]);
+            }
+            else if((strcmp(dici, "n"))==0){
+                continue;
+            }
+            else{
+                printf("Please enter y or n to give command.");
+            }
+        }
+    }
+}
+
+writeData(char *dayName){
+    printf("\nlets create db for %s \n", dayName);
+   
+    printf("How many classes you have on %s ? \n", dayName);
+    
+       int num_of_classes = 0;
+       scanf("%d", &num_of_classes);
+       
+       int i =0; char className[200][200]; char time[200][200];
+       for(i=0; i<=num_of_classes; i++){
+           printf("Name of class number %d \n", i+1);
+           scanf("%s", &className[i]);
+           
+           printf("Time of class number %d \n", i+1);
+           scanf("%s", &time[i]);
+       }
+        
+        FILE *fp = fopen(("%s", dayName), "w");
+        for(i=0; i<=num_of_classes; i++){
+            strcat(className[i], " ");
+            fprintf(fp, className[i]);
+             
+            strcat(time[i], " ");
+            fprintf(fp, "%s", time[i]); 
+        }
+        fclose(fp);
+        printf("\nSaved! %s class scedule will be shown next time you run the app.");
+}
+
+
+readData(char *dayName){
+    FILE *fr;
+    char str[1000];
+    char* filename = dayName;
+ 
+    fr = fopen(filename, "r");
+    char text[1000];
+    while (fgets(str, 1000, fr) != NULL)
+        strcpy(text, str);
+        
+    fclose(fr);
+
+    
+    char *tok; int x = 0;
+    for (tok = strtok(text, " "); tok != NULL; tok = strtok(NULL, " ")){
+        x++;
+        printf("%s", tok);
+        if(x%2==0)
+            printf("\n");
+        else
+            printf("---");
+    }
+}
+
